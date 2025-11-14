70bff2d18c1414886c3ab59d0c30f3e7293752d7
Abdul1432444 <ar9834606@gmail.com>
2025-11-14 10:04:29 +0500

    done

--- reactintro/react-project-two/src/components/Usercard.jsx
+++ reactintro/react-project-two/src/components/Usercard.jsx
@@ -1,6 +1,16 @@
-import app from "react";
-const Usercard = () => {
-  return <div>card</div>;
+import React from "react";
+import "./Usercard.css";
+
+const Usercard = (props) => {
+  return (
+    <div className="card">
+      <p className="card-name">{props.name}</p>
+      <img src={props.image} alt={props.name} />
+      <p className="description">
+        This is a very good-looking <br></br> plant forever.
+      </p>
+    </div>
+  );
 };
 
 export default Usercard;
