b140348a4898cb7d5f626436c4e7b971eb437f6e
Abdul1432444 <ar9834606@gmail.com>
2025-11-14 08:52:51 +0500

    done

--- reactintro/react-project-two/src/App.jsx
+++ reactintro/react-project-two/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import "./App.css";
-import Usercard from "./components/Usercard";
+import Usercard from "./components/UserCard";
+import cherryplant from "./images/cherryplant.jpg";
+import flowers from "./images/flowers.jpg";
+import seaview from "./images/seaview.jpg";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="App">Hello This Is another react app</div>
+      <div className="App">
+        <Usercard name="Cherry" image={cherryplant}></Usercard>
+        <Usercard name="Flowers" image={flowers}></Usercard>
+        <Usercard name="Seaview" image={seaview}></Usercard>
+      </div>
     </>
   );
 }
